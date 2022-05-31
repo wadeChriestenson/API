f7b952d55075b2aa0e720659546a82e71e5cca27
wchriestenson <wadechriestenson@gmail.com>
2022-05-31 13:22:14 -0700

    Created 05/31/22 WC remote

---
+++ README.md
@@ -0,0 +1,7 @@
+# Quick Weather API
+## ***Api used from stormglass.io***
+https://stormglass.io/
+
+API gets
+- airTemperature
+- windSpeed
---
+++ api_data.py
@@ -0,0 +1,27 @@
+import requests
+import arrow
+
+
+# Get first hour of today
+start = arrow.now().floor('day')
+
+# Get last hour of today
+end = arrow.now().ceil('day')
+
+response = requests.get(
+  'https://api.stormglass.io/v2/weather/point',
+  params={
+    'lat': 44.9429,
+    'lng': 123.0351,
+    'params': ','.join(['windSpeed', 'airTemperature']),
+  },
+  headers={
+    'Authorization': 'd52bbf16-e10e-11ec-becb-0242ac130002-d52bbf84-e10e-11ec-becb-0242ac130002'
+  }
+)
+
+# Do something with response data.
+json_data = response.json()
+print(json_data)
+
+# api_key = 'd52bbf16-e10e-11ec-becb-0242ac130002-d52bbf84-e10e-11ec-becb-0242ac130002'
