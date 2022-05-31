15ef31606cbd702363e08c8ba716f75c0f8dacb3
wchriestenson <wadechriestenson@gmail.com>
2022-05-31 14:04:16 -0700

    Created 05/31/22 WC remote

--- api_data.py
+++ api_data.py
@@ -2,11 +2,6 @@ import requests
 import arrow
 
 
-# Get first hour of today
-start = arrow.now().floor('day')
-
-# Get last hour of today
-end = arrow.now().ceil('day')
 
 response = requests.get(
   'https://api.stormglass.io/v2/weather/point',
@@ -22,6 +17,7 @@ response = requests.get(
 
 # Do something with response data.
 json_data = response.json()
+print(response.headers)
 print(json_data)
 
 # api_key = 'd52bbf16-e10e-11ec-becb-0242ac130002-d52bbf84-e10e-11ec-becb-0242ac130002'
